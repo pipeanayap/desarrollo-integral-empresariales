c480b39a85cd0b8215ffefc812e1c20c903f2a97
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2024-08-19 09:34:11 -0600

    Segunda practica

---
+++ Practica2/src/index.ts
@@ -0,0 +1,14 @@
+let miNumero: number = 10;
+
+miNumero = 10 + 456;
+miNumero = 2.5;
+miNumero = 2.5 + 2.5;
+console.log(miNumero);
+
+let booleano:boolean = true;
+booleano= false;
+
+let encendido: boolean;
+
+//console.log(encendido);
+
