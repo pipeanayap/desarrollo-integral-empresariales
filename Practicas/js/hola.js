e8eb2719020e433f7275865a3c94ff3a65938cc0
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2024-08-19 09:13:11 -0600

    Primera practica

---
+++ Practicas/js/hola.js
@@ -0,0 +1,6 @@
+var mensaje = 'Hola TS';
+console.log(mensaje);
+//var let y const
+var numero = 10;
+numero = 3.1416;
+console.log(numero);
---
+++ Practicas/src/Hola.ts
@@ -0,0 +1,8 @@
+let mensaje:string = 'Hola TS';
+console.log(mensaje);
+
+//var let y const
+
+let numero:number = 10;
+numero = 3.1416;
+console.log(numero);
