342c1463f4efb29ac546c45223cc65436e4abd98
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2024-08-20 07:16:56 -0600

    Coreccion

---
+++ Practica2/js/index.js
@@ -0,0 +1,9 @@
+var miNumero = 10;
+miNumero = 10 + 456;
+miNumero = 2.5;
+miNumero = 2.5 + 2.5;
+console.log(miNumero);
+var booleano = true;
+booleano = false;
+var encendido;
+//console.log(encendido);
