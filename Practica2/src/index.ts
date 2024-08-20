f736512975864c1e6f9e3952457bb618f4481a50
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2024-08-20 08:39:59 -0600

    practica2

--- Practica2/src/index.ts
+++ Practica2/src/index.ts
@@ -12,3 +12,21 @@ let encendido: boolean;
 
 //console.log(encendido);
 
+encendido = true;
+
+console.log('Encendido es: ',encendido);
+console.log('Encendido es: ', {encendido});
+
+encendido = true && false;
+console.log(encendido)
+
+encendido = false && true;
+encendido = false && false;
+
+console.log('Resultado', encendido);
+
+let normal:any = 'Hola';
+normal = 10;
+normal = true;
+normal = 11.25;
+console.log('Normal',normal);
