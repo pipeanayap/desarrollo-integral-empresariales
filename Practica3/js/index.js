2edd4ccc037e77325e63d4fef5ce2f377a8a878a
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2024-08-27 08:53:12 -0600

    practica 3

--- Practica3/js/index.js
+++ Practica3/js/index.js
@@ -1,10 +1,9 @@
-"use strict";
-let normalx = 'hola';
+var normalx = 'hola';
 normalx = 10;
 normalx = true;
-let usuario = 21;
+var usuario = 21;
 console.log(normalx);
-let value;
+var value;
 console.log(value);
 //Hola mundo 
 value = true;
@@ -16,26 +15,80 @@ value = Math.random;
 value = null;
 value = undefined;
 console.log(value);
-let myValue;
+var myValue;
 console.log('Tipo desconocido cawn', myValue);
-const edad = 18;
+var edad = 18;
 console.log("edad", edad);
 console.log('edad: ', edad);
-const sueldo = 1.234;
-const total = Number('5678'); //CAST 
+var sueldo = 1.234;
+var total = Number('5678'); //CAST 
 console.log(sueldo);
 console.log(total);
-const personaje = "el chavo";
-const otroPersonaje = "Jirafales";
-const otroMas = `Profesor jirafales y ${personaje}`;
+var personaje = "el chavo";
+var otroPersonaje = "Jirafales";
+var otroMas = "Profesor jirafales y ".concat(personaje);
 console.log(otroMas);
-const heroe = 'deadpool\'s house';
+var heroe = 'deadpool\'s house';
 console.log(heroe);
 console.log(heroe.toUpperCase());
-let vengador;
+var vengador;
 vengador = 'Dr. strange';
 console.log(vengador.charAt(0));
 vengador = 150.55;
 console.log(vengador.toFixed(1)); //el uno representa cuantos decimales queremos despues del punto
 console.log(vengador.toFixed(1));
 console.log(vengador.toFixed(1));
+var personas = ["Carlos", "Saul", "Luis"];
+console.log(personas);
+console.log(personas[0]);
+console.log(personas[3]);
+var numeros = [1, 2, 3, 4, 5];
+var numeros2 = [6, 7, 8, 9, 10];
+console.log(numeros, numeros2);
+var listaNumeros = document.getElementById("listaNumeros");
+var listaNumeros2 = document.getElementById("listaNumeros2");
+for (var _i = 0, numeros_1 = numeros; _i < numeros_1.length; _i++) {
+    var numero = numeros_1[_i];
+    listaNumeros.innerHTML += "<li>".concat(numero, "</li>");
+}
+for (var _a = 0, numeros2_1 = numeros2; _a < numeros2_1.length; _a++) {
+    var numero2 = numeros2_1[_a];
+    listaNumeros2.innerHTML += "<li>".concat(numero2, "</li>");
+}
+var miArray = ['Carlos', 54, 24, 'Saul', 19, 'Luis', 20];
+console.log(miArray);
+var miArray2 = ['Mate', 'Rayo McQueen', 19, 'Hudson Hornet'];
+console.log(miArray2);
+var miArray3 = [true, 10, 'hola', 20.5];
+console.log(miArray3);
+var miArray4 = [];
+miArray4.push(1);
+miArray4.push(2);
+miArray4.push(3);
+console.log(miArray4.length);
+console.log(miArray4.reverse());
+var miArray5 = [];
+miArray5.push('Carlos');
+miArray5.push(20);
+miArray5.push(true);
+miArray5.push('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDhCRLDorEoL4rXQ5P6ZbeTBIJemh2vakP_A&s');
+miArray5.push('Juan');
+miArray5.push(25);
+miArray5.push('https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/36969.png');
+// Obtener las figuras por ID
+var figura1 = document.getElementById("figura1");
+var figura2 = document.getElementById("figura2");
+// Crear y agregar la primera imagen con su caption
+var imagen1 = document.createElement("img");
+imagen1.src = miArray5[3]; // URL de la imagen 1
+var caption1 = document.createElement("figcaption");
+caption1.textContent = "Carlos"; // Título de la imagen 1
+figura1.appendChild(imagen1);
+figura1.appendChild(caption1);
+// Crear y agregar la segunda imagen con su caption
+var imagen2 = document.createElement("img");
+imagen2.src = miArray5[6]; // URL de la imagen 2
+var caption2 = document.createElement("figcaption");
+caption2.textContent = "Juan"; // Título de la imagen 2
+figura2.appendChild(imagen2);
+figura2.appendChild(caption2);
--- Practica3/src/index.ts
+++ Practica3/src/index.ts
@@ -52,3 +52,65 @@ console.log(vengador.toFixed(1)) //el uno representa cuantos decimales queremos 
 console.log((vengador as number).toFixed(1));
 console.log(<number>vengador.toFixed(1));
 
+let personas:string[] = ["Carlos","Saul","Luis"];
+console.log(personas)
+console.log(personas[0])
+console.log(personas[3])
+
+let numeros:number[] = [1,2,3,4,5];
+let numeros2: Array<number> = [6,7,8,9,10];
+
+console.log(numeros, numeros2);
+
+let listaNumeros : any = document.getElementById("listaNumeros");
+let listaNumeros2 : any = document.getElementById("listaNumeros2");
+
+for (let numero of numeros){
+    listaNumeros.innerHTML += `<li>${numero}</li>`
+}
+
+for (let numero2 of numeros2){
+    listaNumeros2.innerHTML += `<li>${numero2}</li>`
+}
+
+let miArray : (string | number)[] = ['Carlos',54,24,'Saul',19, 'Luis',20];
+console.log(miArray);
+let miArray2 : Array <string | number> = ['Mate', 'Rayo McQueen', 19, 'Hudson Hornet'];
+console.log(miArray2);
+let miArray3 : Array <any> = [true, 10, 'hola', 20.5]
+console.log(miArray3);
+let miArray4 : number[] = [];
+miArray4.push(1);
+miArray4.push(2);
+miArray4.push(3);
+console.log(miArray4.length);
+console.log(miArray4.reverse())
+
+let miArray5 = [];
+miArray5.push('Carlos');
+miArray5.push(20);
+miArray5.push(true);
+miArray5.push('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDhCRLDorEoL4rXQ5P6ZbeTBIJemh2vakP_A&s');
+miArray5.push('Juan');
+miArray5.push(25);
+miArray5.push('https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/36969.png'); 
+
+// Obtener las figuras por ID
+let figura1: any = document.getElementById("figura1");
+let figura2:any = document.getElementById("figura2");
+
+// Crear y agregar la primera imagen con su caption
+let imagen1 : any = document.createElement("img");
+imagen1.src = miArray5[3]; // URL de la imagen 1
+let caption1 = document.createElement("figcaption");
+caption1.textContent = "Carlos"; // Título de la imagen 1
+figura1.appendChild(imagen1);
+figura1.appendChild(caption1);
+
+// Crear y agregar la segunda imagen con su caption
+let imagen2:any = document.createElement("img");
+imagen2.src = miArray5[6]; // URL de la imagen 2
+let caption2 = document.createElement("figcaption");
+caption2.textContent = "Juan"; // Título de la imagen 2
+figura2.appendChild(imagen2);
+figura2.appendChild(caption2);
