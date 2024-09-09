a23436d147ca4857cb868dc42d79165ea066004f
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2024-09-09 08:19:05 -0600

    Actualizacion practica 5

--- Practica5/js/index.js
+++ Practica5/js/index.js
@@ -18,3 +18,33 @@ let [fruta1, fruta2] = frutas;
 console.log(fruta1, fruta2);
 let [frutaA, , , frutaB] = frutas;
 console.log(frutaA, frutaB);
+class Personax {
+    constructor(nombre, edadx) {
+        this.nombrex = nombre;
+        this.edadx = edadx;
+    }
+    getEdadx() {
+        return this.edadx;
+    }
+    setEdad(edadx) {
+        this.edadx = edadx;
+    }
+    getNombrex() {
+        return this.nombrex;
+    }
+    setNombrex(nombrex) {
+        this.nombrex = nombrex;
+    }
+}
+let personaje1 = new Personax("Pipe", 20);
+console.log(personaje1.edadx);
+console.log(personaje1.nombrex);
+personaje1.nombrex = 'Hola';
+personaje1.edadx = 25;
+console.log(personaje1.edadx);
+console.log(personaje1.nombrex);
+let personaje2 = new Personax('Carlos', 35);
+personaje2.setEdad(20);
+personaje2.setNombrex('Juan carlos');
+console.log('La nueva edad de carlos es de ', personaje2.getEdadx());
+console.log('El nuevo nombre de carlos es ', personaje2.getNombrex());
--- Practica5/src/index.ts
+++ Practica5/src/index.ts
@@ -12,7 +12,7 @@ console.log(miVariable);
  //Destructuring
 
  let {firstName1, secondName} = Person1;
- console.log(firstName1, ' ', secondName);
+ console.log(firstName1 , ' ', secondName);
 
 
 
@@ -26,4 +26,50 @@ console.log(miVariable);
 
  let [frutaA, , ,frutaB] = frutas;
  console.log(frutaA, frutaB);
+
+
+ class Personax {
+   nombrex: string;
+   edadx : number;
+   constructor(nombre: string, edadx: number){
+      this.nombrex = nombre;
+      this.edadx = edadx;
+   }
+
+   getEdadx() : number {
+      return this.edadx
+   }
+
+   setEdad(edadx : number): void{
+      this.edadx = edadx;
+   }
+
+   getNombrex() : string{
+      return this.nombrex
+   }
+
+   setNombrex(nombrex : string): void{
+      this.nombrex = nombrex;
+   }
+
+ }
+
+ let personaje1 = new Personax("Pipe", 20);
+ console.log(personaje1.edadx);
+ console.log(personaje1.nombrex);
+ 
+ personaje1.nombrex = 'Hola';
+ personaje1.edadx = 25;
+
+ console.log(personaje1.edadx);
+ console.log(personaje1.nombrex);
+
+ let personaje2 = new Personax('Carlos', 35);
+ personaje2.setEdad(20);
+ personaje2.setNombrex('Juan carlos');
+console.log('La nueva edad de carlos es de ', personaje2.getEdadx());
+console.log('El nuevo nombre de carlos es ',personaje2.getNombrex());
+ 
+
+
 
