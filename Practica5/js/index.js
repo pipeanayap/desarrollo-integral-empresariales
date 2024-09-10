753b27a5ba3fa599f80a20ccec52235e61b183a6
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2024-09-10 07:21:00 -0600

    practica 5

--- Practica5/js/index.js
+++ Practica5/js/index.js
@@ -19,9 +19,11 @@ console.log(fruta1, fruta2);
 let [frutaA, , , frutaB] = frutas;
 console.log(frutaA, frutaB);
 class Personax {
-    constructor(nombre, edadx) {
+    constructor(nombre, edadx, apellidoP, apellidoM) {
         this.nombrex = nombre;
         this.edadx = edadx;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
     }
     getEdadx() {
         return this.edadx;
@@ -35,16 +37,40 @@ class Personax {
     setNombrex(nombrex) {
         this.nombrex = nombrex;
     }
+    getApellidoP() {
+        return this.apellidoP;
+    }
+    setApellidoP(apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+    getApellidoM() {
+        return this.apellidoM;
+    }
+    setApellidoM(apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+    nombreCompleto() {
+        return this.nombrex + " " + this.apellidoP + " " + this.apellidoM + " con la edad de " + this.edadx;
+    }
 }
-let personaje1 = new Personax("Pipe", 20);
+let personaje1 = new Personax("Pipe", 20, 'Anaya', 'Parada');
 console.log(personaje1.edadx);
 console.log(personaje1.nombrex);
-personaje1.nombrex = 'Hola';
+//  personaje1.nombrex = 'Hola';
 personaje1.edadx = 25;
 console.log(personaje1.edadx);
 console.log(personaje1.nombrex);
-let personaje2 = new Personax('Carlos', 35);
+let personaje2 = new Personax('Carlos', 35, 'Serrano', 'algo');
 personaje2.setEdad(20);
 personaje2.setNombrex('Juan carlos');
 console.log('La nueva edad de carlos es de ', personaje2.getEdadx());
 console.log('El nuevo nombre de carlos es ', personaje2.getNombrex());
+console.log(personaje1.nombreCompleto());
+class userAccount {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+const usuarioInterface = new userAccount('Imagine Dragons', 2);
+console.log(usuarioInterface.name, usuarioInterface.id);
--- Practica5/src/index.ts
+++ Practica5/src/index.ts
@@ -31,9 +31,14 @@ console.log(miVariable);
  class Personax {
    nombrex: string;
    edadx : number;
-   constructor(nombre: string, edadx: number){
+   apellidoP: string;
+   apellidoM: string
+
+   constructor(nombre: string, edadx: number, apellidoP : string, apellidoM: string){
       this.nombrex = nombre;
       this.edadx = edadx;
+      this.apellidoP = apellidoP;
+      this.apellidoM = apellidoM;
    }
 
    getEdadx() : number {
@@ -52,24 +57,72 @@ console.log(miVariable);
       this.nombrex = nombrex;
    }
 
+   getApellidoP() : string{
+      return this.apellidoP;
+   }
+
+   setApellidoP(apellidoP : string):void{
+      this.apellidoP = apellidoP;
+   }
+
+   getApellidoM() : string{
+      return this.apellidoM;
+   }
+
+   setApellidoM(apellidoM : string):void{
+      this.apellidoM = apellidoM;
+   }
+
+   nombreCompleto(): string{
+      return this.nombrex + " " + this.apellidoP + " "+this.apellidoM + " con la edad de " + this.edadx   
+   }
+
  }
 
- let personaje1 = new Personax("Pipe", 20);
+ let personaje1 = new Personax("Pipe", 20, 'Anaya', 'Parada');
  console.log(personaje1.edadx);
  console.log(personaje1.nombrex);
  
- personaje1.nombrex = 'Hola';
+//  personaje1.nombrex = 'Hola';
  personaje1.edadx = 25;
 
  console.log(personaje1.edadx);
  console.log(personaje1.nombrex);
 
- let personaje2 = new Personax('Carlos', 35);
+ let personaje2 = new Personax('Carlos', 35, 'Serrano', 'algo');
  personaje2.setEdad(20);
  personaje2.setNombrex('Juan carlos');
 console.log('La nueva edad de carlos es de ', personaje2.getEdadx());
 console.log('El nuevo nombre de carlos es ',personaje2.getNombrex());
+
+console.log(personaje1.nombreCompleto());
  
+//Interface
+interface user{
+   name : string;
+   id : number;
+}
+
+class userAccount{
+   name : string;
+   id : number;
+
+   constructor( name : string, id : number){
+      this.name = name;
+      this.id = id;
+   }
+}
+
+const usuarioInterface : user = new userAccount('Imagine Dragons' , 2);
+console.log(usuarioInterface.name, usuarioInterface.id);
+
+//PARTE DEL RETO 
+
+  /*  Crear al menos 5 clases que van a usar en su proyecto, 3 propiedades, getters y setters y al menos un metodo por clase o 
+      funcion agregar un constructor 
+   */
+
+//IGNORAR POR AHORA
 
 
 
